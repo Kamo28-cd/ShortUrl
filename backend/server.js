2a1086226ec320867a4aa77b71b5716c54032f9d
Kamogelo Molefe <kmolefe@Kamogelos-MacBook-Pro.local>
2023-05-16 11:36:59 +0200

    feat: adding redirect

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ const morgan = require("morgan");
 const dotenv = require("dotenv");
 const { MongoClient } = require("mongodb");
 
+//Url model
+const Url = require("./models/Url");
+
 const app = express();
 
 //this Bodyparse middleware helps handle top sent from an api as an object
@@ -56,6 +59,22 @@ app.use(function (req, res, next) {
   next();
 });
 app.use("/api/url", require("./routes/api/url"));
+
+app.get("/:shortUrl", (req, res) => {
+  const url = req.params.shortUrl;
+
+  if (!url)
+    return res.sendStatus(400).json({ msg: "Error: Please enter a valid URL" });
+
+  Url.findOne({ urlShort: url }).then((urlShort) => {
+    if (!urlShort)
+      return res.sendStatus(400).json({
+        msg: "Error: The URL you are looking for does not exist, please try again",
+      });
+
+    res.redirect(urlShort.urlLong);
+  });
+});
 
 const port = process.env.PORT || 5001;
 server.listen(port, () => `Server started on ${port}`);
